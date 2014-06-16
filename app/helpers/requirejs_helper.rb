a16769f7010802f8b8bddfd1c84732d24619fa3c
jeffrey <ziyinghj@gmail.com>
2014-06-16 14:12:44 +0800

    test

--- app/helpers/requirejs_helper.rb
+++ app/helpers/requirejs_helper.rb
@@ -50,7 +50,10 @@ module RequirejsHelper
           paths = {}
           modules.each { |m| paths[m] = _javascript_path(m).sub /\.js$/,'' }
 
-          bundles.each { |m| paths[m] = _javascript_path(m).sub(/\.js$/,'')||m }
+          bundles.each { |m| paths[m] = _javascript_path(m).sub(/\.js$/,'') }
+
+
+          run_config['test'] = bundles
 
           if run_config.has_key? 'paths'
             # Add paths for assets specified by full URL (on a CDN)
